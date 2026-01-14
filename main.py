2455c07ff98cfb31d173e5f7bfe4d327275ad401
Charles Dillaway <doerayme1224@gmail.com>
2026-01-14 00:54:23 -0700

    added most fixins to new project

---
+++ main.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+
+
+st.title("Sore Sales Project")
+
+# --- Pages Section Setup ---
+
+about_page = st.Page(
+    "templates/about.py",
+    title="About me",
+    icon=":material/account_circle:",
+    default=True,
+)
+
+project_1_page = st.Page(
+    "templates/dashboard.py", title="Sales Dashboard", icon=":material/bar_chart:"
+)
+
+# --- Navigation Session Setup ---
+pn = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [project_1_page],
+    }
+)
+
+
+# --- Running Navigation ---
+pn.run()
+
+
+
+# def main():
+#     print("Hello from store-sales-fake-data!")
+
+
+# if __name__ == "__main__":
+#     main()
